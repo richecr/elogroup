712f61a9e4e8b08b811f14a6a00ff57d45961570
Rich <richelton14@gmail.com>
2021-10-31 13:20:41 -0300

    mobx configured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "antd/dist/antd.css";
+import { Form, Input, Button, Checkbox } from "antd";
 
-function App() {
+import { useTodosStore } from "./providers/Todo/TodoProvider";
+import { useState } from "react";
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const todoStore = useTodosStore();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="form-login">
+      <h1>Teste</h1>
+      {todoStore.records.map((todo) => (
+        <div>
+          <p>{todo.name}</p>
+          <p>{todo.description}</p>
+          <Checkbox value={todo.is_done} disabled>
+            Done
+          </Checkbox>
+        </div>
+      ))}
+      <div>
+        <input
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+        />
+        <input
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+        />
+        <button
+          onClick={() => {
+            todoStore.addTodo(name, description);
+            setName("");
+            setDescription("");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add todo
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
